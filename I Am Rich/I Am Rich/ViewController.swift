5c7443dc02a5e87b7b5d86f76749c290118fdf18
yebinpark <yb507970@dal.ca>
2020-12-07 15:20:01 -0400

    first commit for IAmRichApp

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yebin Park on 2020-12-07.
+//  Copyright © 2020 Yebin Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
