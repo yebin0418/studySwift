ad01342bd78c8b1bfb5973e633bf16a30e0ba60b
yebinpark <yb507970@dal.ca>
2020-12-08 20:53:46 -0400

    initial commit for Dicee App

---
+++ Dicee/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Yebin Park on 2020-12-08.
+//  Copyright © 2020 Yebin Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func UIButton(_ sender: UIButton) {
+    }
+    
+
+}
+
