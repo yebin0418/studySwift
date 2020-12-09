5feb2d5a6350bfafe02d4070704d93ff9c1f6baa
yebinpark <yb507970@dal.ca>
2020-12-09 14:24:11 -0400

    adding functionality that two dice's images are updating by randomly generated numbers.

--- Dicee/Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/Dicee/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //creating variables to generate random number
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    //creating a constant array
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateDiceImages()
     }
-    @IBAction func UIButton(_ sender: UIButton) {
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+
+    }
+    
+    //it will update dice images follow by random number created
+    func updateDiceImages() {
+        //generating random number between 0-5
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        //display correct dice image based on the random number generated
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
     
 
