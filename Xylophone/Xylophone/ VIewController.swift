487e7cd53010bcfd8c025770d2c583e840c49ac8
yebinpark <yb507970@dal.ca>
2020-12-10 23:13:19 -0400

    initial commit - implementing a play sound with AVFoundation

---
+++ Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+//import AudioToolbox
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+    
+  
+
+}
+
