4b37c6d134b3e9467ff0c9060a3c94f2b04d7498
yebinpark <yb507970@dal.ca>
2020-12-21 17:14:36 -0400

    adding functionalities for the question bank and answer

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,11 +22,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+
         
     }
 
-
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        questionNumber = questionNumber + 1
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        
   
     }
     
@@ -40,6 +55,15 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("You got it")
+        }
+        else {
+            print("wrong")
+        }
+  
         
     }
     
